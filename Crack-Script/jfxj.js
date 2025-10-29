af3ae8eb96d63d85397ad34acc1875892ba7b58f
chugao <chugaosmps@gmail.com>
2025-10-29 11:32:32 +0800

    Add jfxj.js for response body manipulation

---
+++ Crack-Script/jfxj.js
@@ -0,0 +1,37 @@
+//Wed Oct 29 2025 03:28:37 GMT+0000 (Coordinated Universal Time)
+//Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
+//Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
+(function () {})();
+var _0x3ae793 = JSON.parse($response.body);
+_0x3ae793 = {
+  "msg": "获取成功",
+  "data": {
+    "phone": {
+      "region": "86",
+      "number": "18888888888"
+    },
+    "avatar": "https://public.ysjf.com/user/avatar/default.png",
+    "phoneChangeCount": 3,
+    "id": "u-000231373237152518144",
+    "nickname": "用户_6347",
+    "roles": [{
+      "appGroup": "LutCamera",
+      "id": "m_tlayoibbPE0n",
+      "permanent": true,
+      "name": "Lut相机专业版",
+      "description": null,
+      "expireTime": "2025-10-31T18:15:53.865+08:00"
+    }, {
+      "appGroup": "MediaStory",
+      "id": "r-000035406530190905344",
+      "name": "闪电分镜个人专业版",
+      "description": null,
+      "expireTime": "2026-01-26T17:33:23.000+08:00"
+    }],
+    "identifications": []
+  },
+  "code": 200
+};
+$done({
+  "body": JSON.stringify(_0x3ae793)
+});
